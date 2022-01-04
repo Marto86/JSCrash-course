e1a6b10488717b3b2ceeec4639d6305ea68748df
Martin Pavlov <65560174+Marto86@users.noreply.github.com>
2022-01-04 20:59:44 +0200

    Working

--- YourAgeInDays/script.js
+++ YourAgeInDays/script.js
@@ -1,2 +1,13 @@
-//alert('Hi');
-//console.log('Hello');
+function ageInDays() {
+    var birthYear = prompt("Which Year Did You Born Dear....??");
+    var ageInDayss = (2022 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You Are ' + ageInDayss + ' Days Old.')
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);    
+    }
+    
+    function reset(){
+        document.getElementById('ageInDays').remove();
+    }
